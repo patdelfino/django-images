d7b85c9b0805b83b5a80ec152d363a4d8b0007b3
patdelfino <mpndelfino@tip.edu.ph>
2025-04-26 11:07:54 +0800

    Changed secret key

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('d458e0cd602bcc084ee3b08382356132')
+SECRET_KEY = os.getenv('b425693c26e8e0c09e64303365bf7385')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
