2ca7e79d149e28d71bfc9eb5c1cdb91bb695c4b9
patdelfino <mpndelfino@tip.edu.ph>
2025-04-26 10:43:18 +0800

    Added secret key

--- core/settings.py
+++ core/settings.py
@@ -15,6 +15,7 @@ from dotenv import load_dotenv
 
 import os
 import dj_database_url
+from django.core.exceptions import ImproperlyConfigured
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,15 +27,12 @@ load_dotenv(BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
-if not SECRET_KEY:
-    raise ImproperlyConfigured("The SECRET_KEY environment variable must be set.")
+SECRET_KEY = os.getenv('d458e0cd602bcc084ee3b08382356132')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', '0').lower() in ['true', 't', '1']
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
-ALLOWED_HOSTS.append('django-images-6qex.onrender.com')  # Add your Render domain
 
 
 # Application definition
